bc1f4b18f147dcb596a7f911ab40570ea36f62c4
Filipe Rocha <filipe.rocha@ufrj.br>
2020-03-17 21:06:22 -0300

    added UK

--- main.m
+++ main.m
@@ -14,6 +14,7 @@ flag_download_table = true;
 
 % updating the table
 if flag_download_table
+    disp('Updating data table...');
     urlwrite('https://covid.ourworldindata.org/data/total_cases.csv','total_cases.csv');
 end
 
@@ -28,6 +29,7 @@ country_brazil = data.Brazil;
 country_china = data.China;
 country_italy = data.Italy;
 country_argentina = data.Argentina;
+country_uk = data.UnitedKingdom;
 
 % converting nan to 0
 total_infections(isnan(total_infections))=0;
@@ -36,6 +38,7 @@ country_brazil(isnan(country_brazil))=0;
 country_china(isnan(country_china))=0;
 country_italy(isnan(country_italy))=0;
 country_argentina(isnan(country_argentina))=0;
+country_uk(isnan(country_uk))=0;
 
 % 1st derivative
 diff_spain = diff(country_spain);
@@ -43,6 +46,7 @@ diff_brazil = diff(country_brazil);
 diff_china = diff(country_china);
 diff_italy = diff(country_italy);
 diff_argentina = diff(country_argentina);
+diff_uk = diff(country_uk);
 
 % 2nd derivative
 diff2_spain = diff(diff_spain);
@@ -50,6 +54,7 @@ diff2_brazil = diff(diff_brazil);
 diff2_china = diff(diff_china);
 diff2_italy = diff(diff_italy);
 diff2_argentina = diff(diff_argentina);
+diff2_uk = diff(diff_uk);
 
 %% plotting
 
@@ -73,7 +78,8 @@ plot(time_base, country_brazil, '-.g', 'LineWidth', conf_linewidth);
 plot(time_base, country_spain, '-.r', 'LineWidth', conf_linewidth);
 plot(time_base, country_italy, '-.b', 'LineWidth', conf_linewidth);
 plot(time_base, country_argentina, '-.m', 'LineWidth', conf_linewidth);
-legend('Brasil','Espanha','Italia', 'Argentina');
+plot(time_base, country_uk, '-.c','LineWidth', conf_linewidth);
+legend('Brasil','Espanha','Italia', 'Argentina', 'UK');
 xlabel('Dia'); ylabel('Infectados'); title('Infectados Local');
 
 % plotting 1st derivative
@@ -85,7 +91,8 @@ plot(time_base_dif, diff_brazil, '-.g', 'LineWidth', conf_linewidth-2);
 plot(time_base_dif, diff_spain, '-.r', 'LineWidth', conf_linewidth-2);
 plot(time_base_dif, diff_italy, '-.b', 'LineWidth', conf_linewidth-2);
 plot(time_base_dif, diff_china, '-.k', 'LineWidth', conf_linewidth-2);
-legend('Brasil','Espanha','Italia', 'China');
+plot(time_base_dif, diff_uk, '-.c', 'LineWidth', conf_linewidth-2);
+legend('Brasil','Espanha','Italia', 'China', 'UK');
 xlabel('Dia'); ylabel('Infectados/dia'); title('Infec./dia');
 
 % plotting 1st derivative
@@ -97,7 +104,8 @@ plot(time_base_dif2, diff2_brazil, '-.g', 'LineWidth', conf_linewidth-2);
 plot(time_base_dif2, diff2_spain, '-.r', 'LineWidth', conf_linewidth-2);
 plot(time_base_dif2, diff2_italy, '-.b', 'LineWidth', conf_linewidth-2);
 plot(time_base_dif2, diff2_china, '-.k', 'LineWidth', conf_linewidth-2);
-legend('Brasil','Espanha','Italia', 'China');
+plot(time_base_dif2, diff2_uk, '-.c', 'LineWidth', conf_linewidth-2);
+legend('Brasil','Espanha','Italia', 'China', 'UK');
 xlabel('Dia'); ylabel('Infectados/dia^2'); title('Infec./dia^2');
 
 
